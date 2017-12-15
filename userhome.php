be52b374d0010dd767508c6c46bf0de1bea2fc7b
Siddharth Mogulla <33462380+Siddharth1325@users.noreply.github.com>
2017-12-14 22:11:22 -0500

    sid updated files

--- userhome.php
+++ userhome.php
@@ -26,6 +26,8 @@ type="text/javascript";e.parentNode.insertBefore($,e)})(document,"script");
     <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <script src="bootstrap/js/jquery-1.12.2.js"></script>
     <script src="bootstrap/js/bootstrap.min.js"></script>
+	
+	 
 
     <title> Dashboard </title>
 </head>
@@ -52,7 +54,7 @@ type="text/javascript";e.parentNode.insertBefore($,e)})(document,"script");
 
         <!-- Logo -->
         <div class="navbar-header">
-            <a hre="#" class="navbar-brand" />BLACKBOARD SYSTEM </a>
+            <a hre="#" class="navbar-brand" />BLACKBOARD</a>
         </div>
 
         <!-- Menu on Left -->
@@ -173,6 +175,36 @@ type="text/javascript";e.parentNode.insertBefore($,e)})(document,"script");
                     <div id="collapseThree" class="panel-collapse collapse">
                         <div class="panel-body">
                             <table class="table">
+							<tr>
+                                    <td>
+                                        <li class="active"><a href="#" data-target-id="5"><i></i>Database Systems</a></li>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <li class="active"><a href="#" data-target-id="6"><i></i>Computer Architecture-|</a></li>
+                                    </td>
+                                </tr>
+								<tr>
+                                    <td>
+                                        <li class="active"><a href="#" data-target-id="5"><i></i>Computer Networks</a></li>
+                                    </td></tr>
+									<tr>
+                                    <td>
+                                        <li class="active"><a href="#" data-target-id="5"><i></i>Software Engineering</a></li>
+                                    </td></tr>
+									<tr>
+                                    <td>
+                                        <li class="active"><a href="#" data-target-id="5"><i></i>Technical English Writing</a></li>
+                                    </td></tr>
+									<tr>
+                                    <td>
+                                        <li class="active"><a href="#" data-target-id="5"><i></i>Programming Languages</a></li>
+                                    </td></tr>
+									<tr>
+                                    <td>
+                                        <li class="active"><a href="#" data-target-id="5"><i></i>Computer Architecture-||</a></li>
+                                    </td></tr>
                             </table>
                         </div>
                     </div>
@@ -186,6 +218,17 @@ type="text/javascript";e.parentNode.insertBefore($,e)})(document,"script");
                     <div id="collapseFoure" class="panel-collapse collapse">
                         <div class="panel-body">
                             <table class="table">
+							 <td>
+                                        <li class="active"><a href="#" data-target-id="5"><i></i>Team Support</a></li>
+                                    </td></tr>
+									<tr>
+                                    <td>
+                                        <li class="active"><a href="#" data-target-id="5"><i></i>Get Product Information</a></li>
+                                    </td></tr>
+									<tr>
+                                    <td>
+                                        <li class="active"><a href="#" data-target-id="5"><i></i>Call-888-91-00010 Student Federal Support</a></li>
+                                    </td></tr>
                             </table>
                         </div>
                     </div>
@@ -225,6 +268,7 @@ type="text/javascript";e.parentNode.insertBefore($,e)})(document,"script");
                </div>
 			   </div>
 			   
+                   
 				<script>
 				$(document).ready(function()
 {
@@ -250,7 +294,175 @@ type="text/javascript";e.parentNode.insertBefore($,e)})(document,"script");
 
 
 <body style="background:url(http://www.anishdua.com/wp-content/uploads/2016/07/Light-Grey-Background-Hd-5-1.jpg)"></body>
+ 
+ <link href="bootstrap/css/bootstrap1.css" rel="stylesheet">
 
+
+<!--footer-->
+<footer class="footer1">
+<div class="container">
+
+<div class="row"><!-- row -->
+            
+                <div class="col-lg-3 col-md-3"><!-- widgets column left -->
+                <ul class="list-unstyled clear-margins"><!-- widgets -->
+                        
+                        	<li class="widget-container widget_nav_menu"><!-- widgets list -->
+                    
+                                <h1 class="title-widget">Useful links</h1>
+                                
+                                <ul>
+                                	<li><a  href="#"><i class="fa fa-angle-double-right"></i> About Us</a></li>
+                                    <li><a  href="#"><i class="fa fa-angle-double-right"></i> Contact Us</a></li>
+                                    <li><a  href="#"><i class="fa fa-angle-double-right"></i> Success Stories</a></li>
+                                    <li><a  href="#"><i class="fa fa-angle-double-right"></i> PG Courses</a></li>
+                                    <li><a  href="#"><i class="fa fa-angle-double-right"></i> Achiever's Batch</a></li>
+                                    <li><a  href="#"><i class="fa fa-angle-double-right"></i>  Regular Batch</a></li>
+                                    <li><a  href="#"><i class="fa fa-angle-double-right"></i>  Test & Discussion</a></li>
+                                    <li><a  href="#"><i class="fa fa-angle-double-right"></i>  Fast Track T & D</a></li>
+                                </ul>
+                    
+							</li>
+                            
+                        </ul>
+                         
+                      
+                </div><!-- widgets column left end -->
+                
+                
+                
+                <div class="col-lg-3 col-md-3"><!-- widgets column left -->
+            
+                <ul class="list-unstyled clear-margins"><!-- widgets -->
+                        
+                        	<li class="widget-container widget_nav_menu"><!-- widgets list -->
+                    
+                                <h1 class="title-widget">Useful links</h1>
+                                
+                                <ul>
+ 									<li><a  href="#"><i class="fa fa-angle-double-right"></i>  Test Series Schedule</a></li>
+                                    <li><a  href="#"><i class="fa fa-angle-double-right"></i>  Postal Coaching</a></li>
+                                    <li><a  href="#"><i class="fa fa-angle-double-right"></i>  PG Dr. Bhatia Books</a></li>
+                                    <li><a  href="#"><i class="fa fa-angle-double-right"></i>  UG Courses</a></li>
+                                    <li><a  href="#"><i class="fa fa-angle-double-right"></i>  Satellite Education</a></li>
+                                    <li><a  href="#"><i class="fa fa-angle-double-right"></i>  Study Centres</a></li>
+                                    <li><a  href="#"><i class="fa fa-angle-double-right"></i>  State P.G. Mocks</a></li>
+                                    <li><a  href="#" target="_blank"><i class="fa fa-angle-double-right"></i> Results</a></li>
+                                    
+                                </ul>
+                    
+							</li>
+                            
+                        </ul>
+                         
+                      
+                </div><!-- widgets column left end -->
+                
+                
+                
+                <div class="col-lg-3 col-md-3"><!-- widgets column left -->
+            
+                <ul class="list-unstyled clear-margins"><!-- widgets -->
+                        
+                        	<li class="widget-container widget_nav_menu"><!-- widgets list -->
+                    
+                                <h1 class="title-widget">Useful links</h1>
+                                
+                                <ul>
+
+
+                <li><a href="#"><i class="fa fa-angle-double-right"></i> Enquiry Form</a></li>
+ 				<li><a href="#"><i class="fa fa-angle-double-right"></i> Online Test Series</a></li>
+				<li><a href="#"><i class="fa fa-angle-double-right"></i> Grand Tests Series</a></li>
+				<li><a href="#"><i class="fa fa-angle-double-right"></i> Subject Wise Test Series</a></li>
+				<li><a href="#"><i class="fa fa-angle-double-right"></i> Smart Book</a></li>
+				<li><a href="#"><i class="fa fa-angle-double-right"></i> Test Centres</a></li>
+                <li><a href="#"><i class="fa fa-angle-double-right"></i>  Admission Form</a></li>
+				<li><a href="#"><i class="fa fa-angle-double-right"></i>  Computer Live Test</a></li>
+
+                                </ul>
+                    
+							</li>
+                            
+                        </ul>
+                         
+                      
+                </div><!-- widgets column left end -->
+                
+                
+                <div class="col-lg-3 col-md-3"><!-- widgets column center -->
+                
+                   
+                    
+                        <ul class="list-unstyled clear-margins"><!-- widgets -->
+                        
+                        	<li class="widget-container widget_recent_news"><!-- widgets list -->
+                    
+                                <h1 class="title-widget">Contact Detail </h1>
+                                
+                                <div class="footerp"> 
+                                
+                                <h2 class="title-median">BLACKBOARD Pvt. Ltd.</h2>
+                                <p><b>Email id:</b> <a href="mailto:info@webenlance.com">info@blackboard.com</a></p>
+                                <p><b>Helpline Numbers </b>
+
+    <b style="color:#ffc106;">(8AM to 5PM):</b>  800-999-8888, 811-991-7878  </p>
+    
+    <p><b>Corp Office / Postal Address</b></p>
+    <p><b>Phone Numbers : </b>7042827160, </p>
+    <p> 011-27568832</p>
+                                </div>
+                                
+                                <div class="social-icons">
+                                
+                                	<ul class="nomargin">
+                                    
+				<a href="https://www.facebook.com/bootsnipp"><i class="fa fa-facebook-square fa-3x social-fb" id="social"></i></a>
+	            <a href="https://twitter.com/bootsnipp"><i class="fa fa-twitter-square fa-3x social-tw" id="social"></i></a>
+	            <a href="https://plus.google.com/+Bootsnipp-page"><i class="fa fa-google-plus-square fa-3x social-gp" id="social"></i></a>
+	            <a href="mailto:bootsnipp@gmail.com"><i class="fa fa-envelope-square fa-3x social-em" id="social"></i></a>
+                                    
+                                    </ul>
+                                </div>
+                    		</li>
+                          </ul>
+                       </div>
+                </div>
+</div>
+</footer>
+<!--header-->
+
+<div class="footer-bottom">
+
+	<div class="container">
+
+		<div class="row">
+
+			<div class="col-xs-30 col-sm-10 col-md-10 col-lg-10">
+
+				<div class="copyright">
+
+					© 1984-2018, Blackboard, All rights reserved
+
+				</div>
+
+			</div>
+
+			<div class="col-xs-30 col-sm-10 col-md-10 col-lg-10">
+
+				<div class="design">
+
+					 
+
+				</div>
+
+			</div>
+
+		</div>
+
+	</div>
+
+</div>
 </body>
 
 </html>
