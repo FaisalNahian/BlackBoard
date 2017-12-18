80fd6e1602439522f9fde4598d45529aab43b6a5
Siddharth Mogulla <33462380+Siddharth1325@users.noreply.github.com>
2017-12-17 22:31:31 -0500

    made a small change

--- adminlogin.php
+++ adminlogin.php
@@ -7,7 +7,7 @@
 	if(isset($_SESSION['login_error']))
 	{
 		
-		$login_error = " Invalid Username or Password!" ;
+		$login_error = " Invalid Username / Password ! " ;
 		
 		session_unset();
 		session_destroy();
@@ -17,8 +17,15 @@
 <html>
 	
 <head>
+	
+	
+	
+
 	<link href="bootstrap/global.css" rel="stylesheet" >
-	<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	 
+
+	
+		<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
 	<script src="bootstrap/js/jquery-1.12.2.js"></script>
 	<script src="bootstrap/js/bootstrap.min.js"></script>
 	<link rel="stylesheet" href="bootstrap/css/datepicker.css">
@@ -49,7 +56,7 @@
 				}
 				else
 				{
-					alert("Passwords Do Not Match!");
+					alert(" Passwords Do Not Match !");
 					return false;
 				}
 				
@@ -61,14 +68,14 @@
     <nav class="navbar navbar-inverse">	
 		<div class="container">			
 			<div class="navbar-header">
-				<a href="index.php" class="navbar-brand" />Blackboard</a>
+				<a href="#" class="navbar-brand" />Blackboard</a>
 			</div>	
 
 	        <div id="navbar" class="navbar-collapse collapse">
 	        	<ul class="nav navbar-nav">
 					<li class="active"><a href="index.php"><span class="glyphicon glyphicon-home"></span> Home</a></li>
-					<li><a href="userhome.php"><span class="glyphicon glyphicon-log-in"></span> Students Area</a></li>
-					<li><a href="signup.php"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+					<li><a href="userhome.php"><span class="glyphicon glyphicon-user"></span> Students Area</a></li>
+					<li><a href="signup.php"><span class="glyphicon glyphicon-log-in"></span> Sign-Up</a></li>
 				</ul>
 
 				<!-- Signin form -->
@@ -123,7 +130,6 @@
 		</div>
 
 		<div class="col-xs-4" style="background-color:rgba(202, 205, 205, 0.100) " id="menu">
-<<<<<<< HEAD
 			
 			<div align="center" class="mylogin">
 				<br/>
@@ -131,29 +137,6 @@
 			<br/><span class="text-danger" > </span>
 			<br/><span class="text-danger" > </span>
 			<br/><span class="text-success" > </span>
-=======
-			<?php
-
-				if(isset($_SESSION['success']))
-            	{
-            					?> <div class="alert alert-success">
-                                  <a href="#" class="close" data-dismiss="alert" aria-label="close" hide="true" id="xxx">&times;</a>
-                                  <strong text-align="center">Success: You have been successfully registered!</strong>
-                                </div>
-                                <?php
-                                session_destroy();
-            	}
-
-            ?>
-
-			<div align="center" class="mylogin">
-				<br/>
-			<h1><strong>Instructors Area</strong></h1>
-			<h2>Sign in below</h2>
-			<br/><span class="text-danger" ><?php echo $login_error; ?> </span>
-			<br/><span class="text-danger" ><?php echo $recover_error_var; ?> </span>
-			<br/><span class="text-success" ><?php echo $recover_success_var; ?> </span>
->>>>>>> d90a11aa0e5387967711af54be4eb4d017d47729
 			<form method="post" action="php/adminlogincheck.php" role="form">
 			
 			<input type="text" class="form-control" name="username" placeholder="Username" aria-describedby="basic-addon1" required> <br/><br/>
@@ -180,7 +163,7 @@
 		</div>
 
 
-        <h1>Welcome to Blackboard!</h1>
+      <h1>Welcome to <img src='http://www.utsa.edu/trcss/soar/logos/blackboard.png'/>!!</h1>
         <p>Log-In is ristricted for students</p>
         <p><a class="btn btn-primary btn-lg"     href="signup.php" role="button">Sign Up &raquo;</a>
         <a class="btn btn-primary btn-lg" href="index.php" role="button">Student Area &raquo;</a></p>
--- index.php
+++ index.php
@@ -16,9 +16,9 @@
 ?>
 <html>
 	<head>
-		<meta charset="utf-8">
+		<meta charset="utf-8"><!--
 	    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-	    <meta name="viewport" content="width=device-width, initial-scale=1">
+	    <meta name="viewport" content="width=device-width, initial-scale=1">-->
 	    <meta name="description" content="Blackboard Project for NYIT CSCI 760 Database Course">
 	    <meta name="author" content="Faisal Nahian, Robert Maldonado and Siddharth Mogulla">
 	    <link rel="icon" href="images/favicon.ico">
@@ -67,14 +67,14 @@
      <nav class="navbar navbar-inverse">	
 		<div class="container">			
 			<div class="navbar-header">
-				<a href="index.php" class="navbar-brand" />Blackboard</a>
+				<a href="#" class="navbar-brand" >Blackboard</a>
 			</div>	
 
 	        <div id="navbar" class="navbar-collapse collapse">
 	        	<ul class="nav navbar-nav">
 					<li class="active"><a href="index.php"><span class="glyphicon glyphicon-home"></span> Home</a></li>
-					<li><a href="adminlogin.php"><span class="glyphicon glyphicon-log-in"></span> Instructors Area</a></li>
-					<li><a href="signup.php"><span class="glyphicon glyphicon-user"></span> Sign up</a></li>
+					<li><a href="adminlogin.php"><span class="glyphicon glyphicon-user"></span> Instructors Area</a></li>
+					<li><a href="signup.php"><span class="glyphicon glyphicon-log-in"></span> Sign-Up</a></li>
 				</ul>
 
 				<!-- Signin form -->
@@ -105,8 +105,8 @@
 			<br/><span class="text-success" > </span>
 		</div>
 
-<img src='http://teachingcenter.wustl.edu/wp-content/uploads/2016/01/Blackboard.jpg'/>
-        <h1>Welcome to Blackboard!</h1>
+
+        <h1>Welcome to <img src='http://www.utsa.edu/trcss/soar/logos/blackboard.png'/>!!</h1>
         <p>This site is for students and teachers.</p>
         <p><a class="btn btn-primary btn-lg"     href="signup.php" role="button">Sign Up &raquo;</a>
         <a class="btn btn-primary btn-lg" href="adminlogin.php" role="button">Instructors Area &raquo;</a></p>
